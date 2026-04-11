ac692f526f39abe7ef8b97a006cedc12c3bf5510
Joseph Presley <36678465+jwp23@users.noreply.github.com>
2026-04-10 18:46:23 -0700

    chore: initial commit

--- fs.cc
+++ fs.cc
@@ -1,17 +1,13 @@
 #include "fs.h"
 #include "defer.h"
 
-#include <bit>
 #include <charconv>
 #include <cstring>
 #include <filesystem>
 
-#include <linux/posix_acl_xattr.h>
 #include <sys/file.h>
-#include <sys/mount.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/xattr.h>
 
 bool
 glob(std::string_view pattern, std::string_view target)
@@ -37,19 +33,19 @@ do_fdpath_must(int fd, bool must)
       err("fdpath invalid fd {}", fd);
     return ".";
   }
-  auto procfd = std::format("/proc/self/fd/{}", fd);
-  std::error_code ec;
-  auto res = std::filesystem::read_symlink(procfd, ec);
-  if (ec) {
-    if (must) {
-      errno = ec.value();
-      syserr("{}", procfd);
-    }
-    res = std::format("fd {} [can't determine path]", fd, ec.message());
+  // macOS: use F_GETPATH to resolve fd to a path
+  char pathbuf[MAXPATHLEN];
+  if (fcntl(fd, F_GETPATH, pathbuf) != -1) {
+    path res(pathbuf);
+    if (must && (!res.is_absolute() || !is_fd_at_path(fd, -1, res)))
+      err("{} not valid complete path for fd {}", res.string(), fd);
+    return res;
   }
-  else if (must && (!res.is_absolute() || !is_fd_at_path(fd, -1, res)))
-    err("{} not valid complete path for fd {}", res.string(), fd);
-  return res;
+  if (must) {
+    errno = EBADF;
+    syserr("fcntl(F_GETPATH) for fd {}", fd);
+  }
+  return std::format("fd {} [can't determine path]", fd);
 }
 
 std::string
@@ -57,126 +53,16 @@ fdpath(int fd, const path &file)
 {
   if (fd < 0 || fd == AT_FDCWD || file.is_absolute())
     return file.empty() ? "." : file.string();
-  auto procfd = std::format("/proc/self/fd/{}", fd);
-  std::error_code ec;
-  auto res = std::filesystem::read_symlink(procfd, ec);
-  if (ec)
-    res = std::format("fd {} [can't determine path]: {}", fd, ec.message());
+  // macOS: use F_GETPATH
+  char pathbuf[MAXPATHLEN];
+  path res;
+  if (fcntl(fd, F_GETPATH, pathbuf) != -1)
+    res = pathbuf;
+  else
+    res = std::format("fd {} [can't determine path]", fd);
   if (!file.empty())
     res = res / file;
   return res;
-}
-
-PathMultiset
-mountpoints(const path &mountinfo)
-{
-  const auto mi = read_file(AT_FDCWD, mountinfo);
-  PathMultiset ret;
-
-  for (size_t pos = 0; pos < mi.size();) {
-    size_t start = pos;
-    pos = std::min(mi.find('\n', pos), mi.size() - 1) + 1;
-    std::string_view line(mi.data() + start, pos - start);
-
-    size_t s = 0, e = 0;
-    for (int i = 0; i < 5; ++i) {
-      s = line.find_first_not_of(" ", e);
-      e = line.find(' ', s);
-    }
-    s = std::min(s, line.size());
-    e = std::min(e, line.size());
-    auto field = line.substr(s, e - s);
-
-    std::string mp;
-    for (int i = 0; i < field.size(); ++i) {
-      if (field[i] == '\\' && i + 4 <= field.size()) {
-        const char *p = field.data() + i + 1;
-        char c;
-        auto [eptr, ec] = std::from_chars(p, p + 3, c, 8);
-        if (ec == std::errc{} && eptr == p + 3) {
-          i += eptr - p;
-          mp.push_back(c);
-          continue;
-        }
-      }
-      mp.push_back(field[i]);
-    }
-    if (!mp.empty() && mp[0] == '/')
-      ret.insert(std::move(mp));
-  }
-  return ret;
-}
-
-Fd
-xfsopen(const char *fsname, const char *source)
-{
-  Fd fd = fsopen(fsname, FSOPEN_CLOEXEC);
-  if (!fd)
-    syserr(R"(fsopen("{}"))", fsname);
-  if (source && fsconfig(*fd, FSCONFIG_SET_STRING, "source", source, 0))
-    syserr(R"(fsconfig({}, FSCONFIG_SET_STRING, "source", "{}", 0))", fsname,
-           source);
-  return fd;
-}
-
-Fd
-make_mount(int conffd, int attr)
-{
-  if (fsconfig(conffd, FSCONFIG_CMD_CREATE, nullptr, nullptr, 0))
-    syserr("fsconfig(FSCONFIG_CMD_CREATE)");
-  Fd ret = fsmount(conffd, FSMOUNT_CLOEXEC, attr);
-  if (!ret)
-    syserr("fsmount");
-  return ret;
-}
-
-Fd
-clone_tree(int dfd, const path &file, bool recursive)
-{
-  int flags =
-      AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW | OPEN_TREE_CLOEXEC | OPEN_TREE_CLONE;
-  if (recursive)
-    flags |= AT_RECURSIVE;
-  if (Fd ret = open_tree(dfd, file.c_str(), flags))
-    return ret;
-  syserr(R"(open_tree({}, "{}", 0x{:x}))", fdpath(dfd), file.string(), flags);
-}
-
-void
-xmnt_move(int fromfd, const path &fromfile, int tofd, const path &tofile,
-          int flags)
-{
-  if (move_mount(fromfd, fromfile.c_str(), tofd, tofile.c_str(),
-                 flags | MOVE_MOUNT_F_EMPTY_PATH | MOVE_MOUNT_T_EMPTY_PATH))
-    syserr("move_mount({}, {})", fdpath(fromfd), fdpath(tofd, tofile));
-}
-
-void
-xmnt_setattr(int fd, const path &file, const mount_attr &a, unsigned int flags)
-{
-  flags |= AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW;
-  // Why mount_setattr takes a non-const mount_attr I don't understand...
-  if (mount_setattr(fd, file.c_str(), flags, const_cast<mount_attr *>(&a),
-                    sizeof(a)))
-    syserr("mount_setattr({})", fdpath(fd));
-}
-
-bool
-recursive_umount(const path &tree, bool detach)
-{
-  bool ret = true;
-  auto mps = mountpoints();
-  auto dirs = subtree_rev(mps, tree);
-  for (const auto &dir : dirs) {
-    if (umount2(dir.c_str(), UMOUNT_NOFOLLOW)) {
-      warn(R"(umount("{}"): {})", dir.string(), strerror(errno));
-      if (detach && umount2(dir.c_str(), UMOUNT_NOFOLLOW | MNT_DETACH) == 0)
-        warn("did lazy unmount of {}", dir.string());
-      else
-        ret = false;
-    }
-  }
-  return ret;
 }
 
 bool
@@ -245,104 +131,11 @@ ensure_dir(int dfd, const path &p, mode_t perm, FollowLinks follow,
     auto euid = geteuid();
     if (sb.st_uid != euid)
       err("{}: has uid {} should have {}", p.string(), sb.st_uid, euid);
-    // Because we run with a weird gid
-    if (!euid && sb.st_gid)
-      fchown(*fd, -1, 0);
   }
   if (auto m = sb.st_mode & perm; m != (sb.st_mode & 07777) && fchmod(*fd, m))
     syserr(R"(fchmod("{}", {:o}))", p.string(), m);
   if (created)
     createcb(*fd);
-  return fd;
-}
-
-void
-make_whiteout(int dfd, const path &inp)
-{
-  auto p = inp.lexically_normal();
-  if (!p.has_filename())
-    p = p.parent_path();
-  if (p.is_absolute() || p.empty() || *p.begin() == "..")
-    err<std::logic_error>(R"(make_whiteout: "{}" is not a relative path)",
-                          inp.string());
-
-  try {
-    Fd dirholder;
-    if (p.has_parent_path()) {
-      dirholder = ensure_dir(dfd, p.parent_path(), 0700, kNoFollow, false);
-      dfd = *dirholder;
-      p = p.filename();
-    }
-
-    auto olduid = geteuid();
-    seteuid(0);
-    int err = 0;
-    if (mknodat(dfd, p.filename().c_str(), S_IFCHR, 0))
-      err = errno;
-    seteuid(olduid);
-    if ((errno = err))
-      syserr("mknod {}/.jai c 0 0", fdpath(dfd, p));
-  } catch (const std::system_error &e) {
-    if (e.code() != std::errc::not_a_directory &&
-        e.code() != std::errc::file_exists)
-      throw;
-  }
-}
-
-bool
-is_mountpoint(int dfd, const path &file, FollowLinks follow)
-{
-  struct statx stx;
-  int flags = AT_EMPTY_PATH | AT_NO_AUTOMOUNT;
-  if (follow != kFollow)
-    flags |= AT_SYMLINK_NOFOLLOW;
-  if (statx(dfd, file.c_str(), flags, STATX_BASIC_STATS, &stx))
-    syserr(R"(statx("{}", "{}"))", fdpath(dfd), file.string());
-  if (!(stx.stx_attributes_mask & STATX_ATTR_MOUNT_ROOT))
-    err("statx does not support STATX_ATTR_MOUNT_ROOT");
-  return stx.stx_attributes & STATX_ATTR_MOUNT_ROOT;
-}
-
-Fd
-open_lockfile(int dfd, const path &file)
-{
-  assert(!file.empty());
-
-  Fd fd = openat(dfd, file.c_str(), O_RDWR | O_CLOEXEC | O_NOFOLLOW);
-  if (fd) {
-    if (!flock(*fd, LOCK_EX | LOCK_NB)) {
-      struct stat sb;
-      if (!is_fd_at_path(*fd, dfd, file, kNoFollow, &sb))
-        // Someone may have unlinked after completing setup; fail and
-        // expect the invoker to call again if setup isn't complete.
-        fd.reset();
-      else if (!S_ISREG(sb.st_mode))
-        err("{}: expected regular file", fdpath(dfd, file));
-      return fd;
-    }
-    if (errno != EWOULDBLOCK && errno != EINTR)
-      syserr(R"(flock("{}", LOCK_EX|LOCK_NB))", file.string());
-    // We failed, but delay returning until lock is released, at which
-    // point setup will likely be complete.
-    if (flock(*fd, LOCK_SH) && errno != EINTR)
-      syserr(R"(flock("{}", LOCK_SH))", file.string());
-    fd.reset();
-    return fd;
-  }
-  if (errno != ENOENT)
-    syserr(R"(open("{}"))", file.string());
-
-  path parent = file.parent_path();
-  const char *pp = parent.empty() ? "." : parent.c_str();
-  fd = xopenat(dfd, pp, O_RDWR | O_TMPFILE | O_CLOEXEC, 0600);
-  if (flock(*fd, LOCK_EX | LOCK_NB))
-    // It's a temp file so should be impossible for anyone else to lock it
-    syserr("flock(O_TMPFILE)");
-  if (linkat(*fd, "", dfd, file.c_str(), AT_EMPTY_PATH)) {
-    if (errno != EEXIST)
-      syserr(R"(linkat("{}"))", file.string());
-    fd.reset();
-  }
   return fd;
 }
 
@@ -356,7 +149,6 @@ open_flags_to_string(int flags)
   static constexpr auto composites = std::to_array<Flag>({
       {O_ACCMODE, "3"},
       {O_SYNC, "O_SYNC"},
-      {O_TMPFILE, "O_TMPFILE"},
   });
   static constexpr auto known_flags = std::to_array<Flag>({
       {O_WRONLY, "O_WRONLY"},
@@ -369,14 +161,10 @@ open_flags_to_string(int flags)
       {O_NONBLOCK, "O_NONBLOCK"},
       {O_DSYNC, "O_DSYNC"},
       {O_ASYNC, "O_ASYNC"},
-      {O_DIRECT, "O_DIRECT"},
-      {O_LARGEFILE, "O_LARGEFILE"},
       {O_DIRECTORY, "O_DIRECTORY"},
       {O_NOFOLLOW, "O_NOFOLLOW"},
-      {O_NOATIME, "O_NOATIME"},
       {O_CLOEXEC, "O_CLOEXEC"},
       {O_SYNC, "O_SYNC"},
-      {O_PATH, "O_PATH"},
   });
 
   std::string result;
@@ -385,7 +173,7 @@ open_flags_to_string(int flags)
     result += '|';
   };
 
-  if ((flags & (O_ACCMODE | O_PATH)) == 0)
+  if ((flags & O_ACCMODE) == 0)
     append("O_RDONLY");
 
   for (auto &c : composites)
@@ -462,7 +250,7 @@ ensure_file(int dfd, path file, std::string_view contents, int mode,
       xopenat(dfd, tmp.c_str(), O_CREAT | O_EXCL | O_WRONLY | O_CLOEXEC, mode);
   for (size_t i = 0; i < contents.size();) {
     if (auto n = write(*fd, contents.data() + i, contents.size() - i); n < 0)
-      syserr(R"(write(O_TMPFILE for "{}"))", fdpath(dfd, file));
+      syserr(R"(write(tmp for "{}"))", fdpath(dfd, file));
     else
       i += n;
   }
@@ -477,178 +265,3 @@ ensure_file(int dfd, path file, std::string_view contents, int mode,
   createcb(*fd);
   return fd;
 }
-
-std::optional<XattrVal>
-xfgetxattr(int fd, const char *attrname, size_t initial_size)
-{
-  auto ret = std::optional(XattrVal{});
-  ret->resize(initial_size);
-  for (;;) {
-    auto n = fgetxattr(fd, attrname, ret->data(), ret->size());
-    if (n < 0) {
-      if (errno == ENODATA) {
-        ret.reset();
-        return ret;
-      }
-      if (errno != ERANGE || ret->empty())
-        syserr(R"({}: fgetxattr("{}"))", fdpath(fd), attrname);
-      ret->clear(); // will retrieve the size next time
-    }
-    else if (n > ret->size())
-      ret->resize(n);
-    else {
-      ret->resize(n);
-      return ret;
-    }
-  }
-}
-
-void
-xfsetxattr(int fd, const char *attrname, std::span<const std::byte> val,
-           int flags)
-{
-  if (fsetxattr(fd, attrname, val.data(), val.size(), flags))
-    syserr(R"({}: fsetxattr("{}"))", fdpath(fd), attrname);
-}
-
-namespace acl {
-
-template<typename T> requires std::is_trivially_copyable_v<T>
-inline void
-pushbytes(XattrVal &v, const T &t)
-{
-  auto p = reinterpret_cast<const std::byte *>(&t);
-  v.insert(v.end(), p, p + sizeof(t));
-}
-
-inline auto
-loadle(std::integral auto i)
-{
-  if constexpr (std::endian::native != std::endian::little)
-    return std::byteswap(i);
-  else
-    return i;
-}
-
-template<std::integral I>
-inline void
-storele(I &dst, std::integral auto src)
-{
-  dst = loadle(I(src));
-}
-
-XattrVal
-serialize(const ACL &a)
-{
-  std::vector<std::byte> ret;
-
-  posix_acl_xattr_header h{};
-  storele(h.a_version, POSIX_ACL_XATTR_VERSION);
-  pushbytes(ret, h);
-
-  for (const auto &e : a) {
-    posix_acl_xattr_entry le{};
-    storele(le.e_tag, e.tag);
-    storele(le.e_perm, e.perm);
-    storele(le.e_id, e.id);
-    pushbytes(ret, le);
-  }
-
-  return ret;
-}
-
-template<typename T> requires std::is_trivially_copyable_v<T>
-struct unaligned {
-  std::byte storage_[sizeof(T)];
-  static constexpr size_t size()
-  {
-    static_assert(sizeof(unaligned) == sizeof(T));
-    return sizeof(T);
-  }
-};
-
-template<typename T>
-T
-get(const unaligned<T> *p)
-{
-  T ret;
-  memcpy(&ret, p, sizeof(T));
-  return ret;
-}
-
-ACL
-deserialize(const XattrVal &raw)
-{
-  using Hdr = unaligned<posix_acl_xattr_header>;
-  using Ent = unaligned<posix_acl_xattr_entry>;
-
-  if (raw.size() < Hdr::size() || ((raw.size() - Hdr::size()) % Ent::size()))
-    err("acl::deserialize: invalid size {} bytes", raw.size());
-
-  auto h = get(reinterpret_cast<const Hdr *>(raw.data()));
-  if (auto v = loadle(h.a_version); v != POSIX_ACL_XATTR_VERSION)
-    err("acl::deserialize: invalid version {}", v);
-
-  size_t nentries = (raw.size() - Hdr::size()) / Ent::size();
-  auto entries = reinterpret_cast<const Ent *>(raw.data() + Hdr::size());
-
-  ACL ret;
-  ret.reserve(nentries);
-  for (const Ent *ent = entries, *end = entries + nentries; ent < end; ++ent) {
-    posix_acl_xattr_entry re = get(ent);
-    auto tag = loadle(re.e_tag);
-    auto id = Entry::has_id(tag) ? loadle(re.e_id) : ACL_UNDEFINED_ID;
-    ret.emplace_back(tag, id, loadle(re.e_perm));
-  }
-  return ret;
-}
-
-std::optional<ACL>
-fdgetacl(int fd, AclName which)
-{
-  if (auto raw = xfgetxattr(fd, which.name))
-    return deserialize(*raw);
-  return std::nullopt;
-}
-
-void
-fdsetacl(int fd, const ACL &val, AclName which, int flags)
-{
-  auto raw = serialize(val);
-  if (fsetxattr(fd, which.name, raw.data(), raw.size(), flags))
-    syserr("{}: fdsetacl({}, {})", fdpath(fd), val, flags);
-}
-
-struct tagid {
-  uint16_t tag;
-  uint32_t id = ACL_UNDEFINED_ID;
-  tagid(uint16_t t) noexcept : tag(t) { assert(!Entry::has_id(tag)); }
-  tagid(const Entry &e) noexcept
-    : tag(e.tag), id(e.has_id() ? e.id : ACL_UNDEFINED_ID)
-  {}
-  friend auto operator<=>(const tagid &a, const tagid &b) noexcept = default;
-};
-
-ACL
-normalize(const ACL &a)
-{
-  std::map<tagid, Entry> amap;
-  for (const auto &e : a)
-    amap.insert_or_assign(e, e);
-
-  // owner, filegroup, other, and mask must all exist
-  for (uint16_t tag : {ACL_USER_OBJ, ACL_GROUP_OBJ, ACL_OTHER})
-    amap.try_emplace(tag, Entry{tag, 0});
-  if (!amap.contains(ACL_MASK)) {
-    uint16_t bound = 0;
-    for (const auto &[_ti, e] : amap)
-      if (e.tag & (ACL_USER | ACL_GROUP_OBJ | ACL_GROUP))
-        bound |= e.perm;
-    amap.insert_or_assign(ACL_MASK, mask(bound));
-  }
-
-  auto vals = amap | std::views::values;
-  return ACL(vals.begin(), vals.end());
-}
-
-} // namespace acl
--- fs.h
+++ fs.h
@@ -19,8 +19,6 @@
 
 #include <dirent.h>
 #include <fcntl.h>
-#include <linux/posix_acl.h>
-#include <sys/mount.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -62,29 +60,6 @@ using PathSet = std::set<path, PathLess>;
 using PathMultiset = std::multiset<path, PathLess>;
 template<typename V> using PathMap = std::map<path, V, PathLess>;
 
-// Return a range for a subtree rooted at root.  root itself will be
-// returned only if it does not contain a trailing slash.
-inline auto
-subtree(const is_one_of<PathSet, PathMultiset> auto &s, const path &root)
-{
-  if (root.relative_path().empty())
-    return std::ranges::subrange(s.begin(), s.end());
-  path end = root;
-  if (end.filename().empty())
-    end = end.parent_path();
-  // First possible pathname not under root is the (illegal) pathname
-  // in which root's final component has a '\0' byte appended.
-  end += '\0';
-  return std::ranges::subrange(s.lower_bound(root), s.lower_bound(end));
-}
-
-// Return a subtree in reverse order (suitable for unmounting).
-inline auto
-subtree_rev(const is_one_of<PathSet, PathMultiset> auto &s, const path &root)
-{
-  return subtree(s, root) | std::views::reverse;
-}
-
 std::string fdpath(int fd, const path &file);
 
 inline std::string
@@ -98,64 +73,6 @@ fdpath(int fd)
 {
   return fdpath(fd, false);
 }
-
-PathMultiset mountpoints(const path &mountinfo = "/proc/self/mountinfo");
-
-// source (if non-NULL) is the source printed in /proc/self/mountinfo
-Fd xfsopen(const char *fsname, const char *source = nullptr);
-
-// Calls fsconfig(FSCONFIG_CMD_CREATE) and fsmount.
-Fd make_mount(int conffd, int attr = MOUNT_ATTR_NOSUID | MOUNT_ATTR_NODEV);
-
-Fd clone_tree(int dfd, const path &file = {}, bool recursive = false);
-
-void xmnt_move(int fromfd, const path &frompath, int tofd, const path &topath,
-               int flags);
-inline void
-xmnt_move(int fromfd, int tofd, const path &topath = {}, int flags = 0)
-{
-  xmnt_move(fromfd, path{}, tofd, topath, flags);
-}
-
-void xmnt_setattr(int fd, const path &file, const mount_attr &a,
-                  unsigned int flags = AT_RECURSIVE);
-inline void
-xmnt_setattr(int fd, const mount_attr &a, unsigned int flags = AT_RECURSIVE)
-{
-  xmnt_setattr(fd, {}, a, flags);
-}
-
-inline void
-xmnt_propagate(int fd, std::uint64_t propagation, bool recursive = true)
-{
-  mount_attr a{.propagation = propagation};
-  xmnt_setattr(fd, a, recursive ? AT_RECURSIVE : 0);
-}
-
-inline void
-xmnt_propagate(int fd, path file, std::uint64_t propagation,
-               bool recursive = true)
-{
-  mount_attr a{.propagation = propagation};
-  xmnt_setattr(fd, file, a, recursive ? AT_RECURSIVE : 0);
-}
-
-template<std::convertible_to<const char *>... Opt>
-requires (sizeof...(Opt) % 2 == 0)
-Fd
-make_tmpfs(const char *source, Opt... opt)
-{
-  Fd conf = xfsopen("tmpfs", source);
-  if constexpr (sizeof...(Opt)) {
-    auto options = std::to_array<const char *>({opt...});
-    for (auto i = 0uz; i < options.size() - 1; i += 2)
-      if (fsconfig(*conf, FSCONFIG_SET_STRING, options[i], options[i + 1], 0))
-        syserr(R"(fsconfig(tmpfs, "{}", "{}"))", options[i], options[i + 1]);
-  }
-  return make_mount(*conf);
-}
-
-bool recursive_umount(const path &tree, bool detach = true);
 
 enum class FollowLinks {
   kNoFollow = 0,
@@ -176,56 +93,6 @@ using CreateCB = std::function<void(int)>;
 Fd ensure_dir(
     int dfd, const path &p, mode_t perm, FollowLinks follow,
     bool okay_if_other_owner = false, CreateCB createcb = [](int) {});
-
-void make_whiteout(int dfd, const path &p);
-
-bool is_mountpoint(int dfd, const path &file = {},
-                   FollowLinks follow = kNoFollow);
-
-// Open an exclusive lockfile to guard one-time setup.  Might fail, in
-// which case re-check the need for setup and try again.
-Fd open_lockfile(int dfd, const path &file);
-
-// If validate(get()) is true, returns the result of get() in the
-// expected value.  Otherwise, acquires the lock and returns an error
-// value containing a Defer object that releases the lock.
-//
-// Be careful not to destroy the return value.  This would be bad:
-//
-//    if (auto r = lock_or_validate(...); r)
-//      return std::move(*r);
-//    // now you no longer have the lock
-//
-// Instead you want:
-//
-//    auto r = lock_or_validate(...);
-//    if (r)
-//      return std::move(*r);
-//    // now you continue to hold the lock until r is destroyed
-template<typename Get,
-         typename Validate = decltype([](auto &&v) { return bool(v); })>
-std::expected<std::decay_t<std::invoke_result_t<Get>>, Defer>
-lock_or_validate(int dfd, path lockfile, Get get, Validate validate = {})
-    requires requires {
-      { validate(get()) } -> std::convertible_to<bool>;
-    }
-{
-  std::expected<std::decay_t<std::invoke_result_t<Get>>, Defer> ret =
-      std::unexpected{Defer{}};
-
-  Fd lock;
-  for (;;) {
-    if (validate(ret.emplace(get())))
-      return ret;
-    if (lock) {
-      ret = std::unexpected{Defer{[lock = std::move(lock), dfd, lockfile] {
-        unlinkat(dfd, lockfile.c_str(), 0);
-      }}};
-      return ret;
-    }
-    lock = open_lockfile(dfd, lockfile);
-  }
-}
 
 std::string open_flags_to_string(int flags);
 
@@ -292,8 +159,11 @@ inline std::array<Fd, 2>
 xpipe()
 {
   int fds[2];
-  if (pipe2(fds, O_CLOEXEC))
-    syserr("pipe2");
+  if (pipe(fds))
+    syserr("pipe");
+  // Set close-on-exec for both ends
+  fcntl(fds[0], F_SETFD, FD_CLOEXEC);
+  fcntl(fds[1], F_SETFD, FD_CLOEXEC);
   return {fds[0], fds[1]};
 }
 
@@ -338,153 +208,3 @@ create_warn(int fd)
 Fd ensure_file(
     int dfd, path file, std::string_view contents, int mode = 0600,
     CreateCB createcb = [](int) {});
-
-using XattrVal = std::vector<std::byte>;
-
-std::optional<XattrVal> xfgetxattr(int fd, const char *attrname,
-                                   size_t initial_size = 64);
-
-void xfsetxattr(int fd, const char *attrname, std::span<const std::byte> val,
-                int flags = 0);
-
-namespace acl {
-
-struct Perm {
-  uint16_t val{};
-  constexpr Perm(int v) : val(v) {}
-  consteval Perm(const char *s)
-  {
-    while (*s)
-      switch (*s++) {
-      case 'r':
-        val |= ACL_READ;
-        break;
-      case 'w':
-        val |= ACL_WRITE;
-        break;
-      case 'x':
-        val |= ACL_EXECUTE;
-        break;
-      case '-':
-        break;
-      default:
-        throw "invalid permission string";
-      }
-  }
-};
-
-struct Entry {
-  uint16_t tag;
-  uint16_t perm;
-  uint32_t id = ACL_UNDEFINED_ID;
-
-  constexpr Entry(uint16_t t, Perm p) noexcept : tag(t), perm(p.val) {}
-  constexpr Entry(uint16_t t, uint32_t i, Perm p) noexcept
-    : tag(t), perm(p.val), id(i)
-  {
-    assert(has_id() || id == ACL_UNDEFINED_ID);
-  }
-
-  static constexpr char tag_char(uint16_t tag)
-  {
-    switch (tag) {
-    case ACL_USER_OBJ:
-    case ACL_USER:
-      return 'u';
-    case ACL_GROUP_OBJ:
-    case ACL_GROUP:
-      return 'g';
-    case ACL_MASK:
-      return 'm';
-    case ACL_OTHER:
-      return 'o';
-    default:
-      return 0;
-    }
-  }
-  char tag_char() const { return tag_char(tag); }
-
-  static constexpr bool has_id(uint16_t tag)
-  {
-    return tag == ACL_USER || tag == ACL_GROUP;
-  }
-  bool has_id() const { return has_id(tag); }
-
-  template<uint16_t Tag> requires (has_id(Tag))
-  static Entry make(uint32_t i, Perm p)
-  {
-    return {Tag, i, p};
-  }
-  template<uint16_t Tag> requires (!has_id(Tag))
-  static Entry make(Perm p)
-  {
-    return {Tag, p};
-  }
-
-  friend auto operator<=>(const Entry &, const Entry &) noexcept = default;
-};
-
-constexpr auto owner = Entry::make<ACL_USER_OBJ>;
-constexpr auto fgroup = Entry::make<ACL_GROUP_OBJ>; // "file group"
-constexpr auto other = Entry::make<ACL_OTHER>;
-constexpr auto uid = Entry::make<ACL_USER>;
-constexpr auto gid = Entry::make<ACL_GROUP>;
-constexpr auto mask = Entry::make<ACL_MASK>;
-
-using ACL = std::vector<Entry>;
-
-XattrVal serialize(const ACL &a);
-ACL deserialize(const XattrVal &raw);
-
-struct AclName {
-  const char *const name;
-  consteval explicit AclName(const char *n) : name(n) {}
-};
-// ACL on an inode
-constexpr AclName kAclAccess("system.posix_acl_access");
-// ACL for newly created files in a directory
-constexpr AclName kAclDefault("system.posix_acl_default");
-
-std::optional<ACL> fdgetacl(int fd, AclName which = kAclAccess);
-void fdsetacl(int fd, const ACL &val, AclName which = kAclAccess,
-              int flags = 0);
-
-// Remove duplicate entries (last one wins), make sure owner, fgroup,
-// other, and mask are present.
-ACL normalize(const ACL &a);
-
-} // namespace acl
-
-template<> struct std::formatter<acl::Entry> {
-  using entry_type = acl::Entry;
-  constexpr auto parse(auto &ctx) { return ctx.begin(); }
-  auto format(const entry_type &e, auto &ctx) const
-  {
-    auto out = ctx.out();
-    if (char c = e.tag_char())
-      out = std::format_to(out, "{}:", c);
-    else
-      out = std::format_to(out, "TAG#{}:", e.tag);
-    if (e.has_id())
-      out = std::format_to(out, "{}", e.id);
-    else if (e.id != ACL_UNDEFINED_ID)
-      out = std::format_to(out, "[{}]", e.id);
-    return std::format_to(out, ":{}{}{}", e.perm & ACL_READ ? 'r' : '-',
-                          e.perm & ACL_WRITE ? 'w' : '-',
-                          e.perm & ACL_EXECUTE ? 'x' : '-');
-  }
-};
-
-template<> struct std::formatter<acl::ACL> : std::formatter<std::string> {
-  using super = std::formatter<std::string>;
-  auto format(const acl::ACL &a, auto &ctx) const
-  {
-    std::string buf;
-    for (const auto &e : a) {
-      if (!buf.empty())
-        buf += ',';
-      buf += std::format("{}", e);
-    }
-    return std::format_to(ctx.out(), "{}", buf);
-  }
-};
