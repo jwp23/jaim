abc37b45affffbd8a2d60fde4e566f3f286efa3c
Joseph Presley <36678465+jwp23@users.noreply.github.com>
2026-04-10 21:19:20 -0700

    docs: add GPL3 license headers to source files
    COPYING declares GPL3 licensing but source files lacked the required
    prominent notices. Per GPL3 section 5, modified files now carry a
    notice stating the modification (macOS arm64 port) and its author
    (Joseph Presley), alongside the original David Mazieres copyright.
    
    Also credit Joseph Presley in AUTHORS for the macOS arm64 port.

--- jaim.cc
+++ jaim.cc
@@ -1,3 +1,24 @@
+/*
+ * jaim - Sandboxing tool for AI CLI access
+ * Copyright (C) 2026 David Mazieres
+ * Copyright (C) 2026 Joseph Presley
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Modified 2026 by Joseph Presley: port from Linux to macOS arm64.
+ */
+
 #include "jaim.h"
 #include "fs.h"
 
